12e97ca5c4b98a7f77152d948687e50cc492b6d8
kaua <goncalveskaua2k24@gmail.com>
2025-05-13 17:43:18 -0300

    testes em python

---
+++ cod.py
@@ -0,0 +1,9 @@
+faturamento = 1000
+custo = 346
+
+novas_vendas = 300
+faturamento = faturamento + novas_vendas
+
+print('fsturamento', faturamento)
+print('custo', custo)
+print('lucro', faturamento - custo)
